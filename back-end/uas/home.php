19d0a78dbb108410c0f873aef2ee7e4598d18a2a
mdepraba <128952605+madepraba@users.noreply.github.com>
2023-09-05 14:51:49 +0800

    Add files via upload

---
+++ back-end/uas/home.php
@@ -0,0 +1,32 @@
+<?php 
+    require_once "classes/login_class.php";
+    $log->access();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="src/navbar.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+    <?php include "src/navbar.php";    ?>
+</head>
+<body>
+
+    
+    <h1 class="d-flex justify-content-center align-items-center">Selamat Datang</h1>
+
+
+</body>
+<style>
+    h1{
+        margin-top: 300px;
+    }
+</style>
+</html>
