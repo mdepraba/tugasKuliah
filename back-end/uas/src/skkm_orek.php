9be720cf10c4cdd94179a3980fa7e3e30ccaebc2
mdepraba <128952605+madepraba@users.noreply.github.com>
2023-09-06 19:47:56 +0800

    Create skkm_orek.php

---
+++ back-end/uas/src/skkm_orek.php
@@ -0,0 +1,17 @@
+<?php
+value = 0;
+
+switch($_POST['name']) {
+  case wajib :
+  switch($_POST['id']) {
+    case gmti :
+    case kulindus1th :
+    case kulindus2th :
+
+    
+  }
+  break:
+  case bidang_ilmiah :
+  case bidang_minat :
+  case bidang_organisasi :
+}
